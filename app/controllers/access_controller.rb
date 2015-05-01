aa76c47b62c169c7314063e1781570127b4508b2
Nick Nathan <nrn2@georgetown.edu>
2015-05-01 12:40:21 -0400

    adding login form

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -21,7 +21,7 @@ class AccessController < ApplicationController
   	if authorized_user
   		session[:user_id] = authorized_user.id
   		session[:email] = authorized_user.email
-  		flash[:notice] = "You are now logged in."
+  		flash[:notice] = "You are now signed in."
   		redirect_to(:controller => 'main', :action => 'index')
   	else
   		flash[:notice] = "Invalid email/password combination."
@@ -32,7 +32,7 @@ class AccessController < ApplicationController
   def logout
   	session[:user_id] = nil
   	session[:email] = nil
-  	flash[:notice] = "Logged out"
+  	flash[:notice] = "Signed out"
   	redirect_to(:action => "login")
   end
 
